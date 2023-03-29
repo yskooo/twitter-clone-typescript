3ea113b89564438e7290cec745c5eb7d7cc56f12
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-03-29 16:14:55 +0800

    chore: create registermodal component

---
+++ components/modals/RegisterModal.tsx
@@ -0,0 +1,100 @@
+import { useCallback, useState } from 'react';
+
+import useLoginModal from '../../hooks/useLoginModal';
+import useRegisterModal from '../../hooks/useRegisterModal';
+
+import Input from '../Input';
+import Modal from '../Modal';
+
+const RegisterModal = () => {
+    const loginModal = useLoginModal();
+    const registerModal = useRegisterModal();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onToggle = useCallback(() => {
+        if (isLoading) {
+            return;
+        }
+
+        registerModal.onClose();
+        loginModal.onOpen();
+    }, [isLoading, registerModal, loginModal])
+
+    const onSubmit = useCallback(async () => {
+        try {
+            setIsLoading(true);
+
+            // TODO ADD REGISTER AND LOG IN
+
+            registerModal.onClose();
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false);
+        }
+    }, [registerModal]);
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Input 
+                placeholder='Email'
+                onChange={(e)=> setEmail(e.target.value)}
+                value={email}
+                disabled={isLoading}
+            />
+            <Input 
+                placeholder='Name'
+                onChange={(e)=> setName(e.target.value)}
+                value={name}
+                disabled={isLoading}
+            />
+            <Input 
+                placeholder='Username'
+                onChange={(e)=> setUsername(e.target.value)}
+                value={username}
+                disabled={isLoading}
+            />
+            <Input 
+                placeholder='Password'
+                onChange={(e)=> setPassword(e.target.value)}
+                value={password}
+                disabled={isLoading}
+            />
+        </div>
+    )
+
+    const footerContent = (
+        <div className="text-neutral-400 text-center mt-4">
+            <p>Alreay have an account? 
+                <span
+                    onClick={onToggle}
+                    className='
+                        text-white
+                        cursor-pointer
+                        hover:underline
+                    '
+                > Sign In </span>
+            </p>
+        </div>
+    )
+
+     return (
+       <Modal
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title='Create an account'
+            actionLabel='Register'
+            onClose={registerModal.onClose}
+            onSubmit={onSubmit}
+            body={bodyContent}
+            footer={footerContent}
+       />
+    )
+}
+
+export default RegisterModal;
