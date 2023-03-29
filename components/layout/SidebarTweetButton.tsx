bf82a399c6f129cc78a1647b783b321b71c8ac90
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-03-29 16:14:01 +0800

    update: sidebartweetbutton component
    add onclick function from tweet button to modal

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,11 +1,20 @@
 import { useRouter } from "next/router";
+import { useCallback } from "react";
+
 import { FaFeather } from "react-icons/fa";
+
+import useLoginModal from "../../hooks/useLoginModal";
 
 const SidebarTweetButton = () => {
     const router = useRouter();
+    const loginModal = useLoginModal();
 
+    const onClick = useCallback(() => {
+        loginModal.onOpen();
+    }, [loginModal]);
+    
     return (
-        <div onClick={() => router.push('/')}>
+        <div onClick={onClick}>
             <div className="
                     mt-6
                     lg:hidden
