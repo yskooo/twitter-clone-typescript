b8575886428779341e4cae78b8a5b5837102ae07
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-03-31 11:26:57 +0800

    next cache stuffs how do u stop these lol

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e67b19735204dcb"; }
+/******/ 		__webpack_require__.h = function() { return "c37795bae33984b0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
