bbfff2c3d5ff63396ab2afaa7d19da64d789f4ca
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-01 07:43:19 +0800

    add sign in, credentials

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,20 +1,41 @@
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
 import {IconType} from 'react-icons';
+import useCurrentUser from '../../hooks/useCurrentUser';
+import useLoginModal from '../../hooks/useLoginModal';
 
 interface SidebarItemProps {
-    label: string,
-    href: string,
+    label: string;
+    href: string;
     icon: IconType
-    onClick?: () => void
+    onClick?: () => void;
+    auth?: boolean;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
     label,
     href,
     icon: Icon,
-    onClick
+    onClick,
+    auth
 }) => {
+  const loginModal = useLoginModal();
+  const { data: currentUser } = useCurrentUser();
+  const router = useRouter();
+  const handleClick = useCallback(() => {
+    if (onClick) {
+      return onClick();
+    }
+
+    if (auth && !currentUser) {
+      loginModal.onOpen();
+    } else if (href) {
+       router.push(href);
+    }
+  }, [router, onClick, href, currentUser, auth, loginModal]);
+
   return (
-    <div className='flex flex-row items-center'>
+    <div onClick={handleClick} className='flex flex-row items-center'>
       <div 
         className="
             relative
