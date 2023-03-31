90be8dd0d4aadf09b3606d2d5513f0426207990b
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-01 07:47:12 +0800

    chore: add session provider, toast, and auth modals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
 
 import Layout from '../components/Layout';
 import LoginModal from '../components/modals/LoginModal';
@@ -7,12 +9,13 @@ import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-    <RegisterModal/>
-    <LoginModal/>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <SessionProvider session={pageProps.session}>
+      <Toaster/>
+      <RegisterModal/>
+      <LoginModal/>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+    </SessionProvider>
   ) 
 }
