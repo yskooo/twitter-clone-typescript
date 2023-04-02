90a21c3d9bf2320ac28b0235cf0244763034fc27
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-02 22:09:35 +0800

    create edit profile modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,15 @@ import { SessionProvider } from 'next-auth/react';
 import Layout from '../components/Layout';
 import LoginModal from '../components/modals/LoginModal';
 import RegisterModal from '../components/modals/RegisterModal';
+import EditModal from '../components/modals/EditModal';
+
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster/>
+      <EditModal/>
       <RegisterModal/>
       <LoginModal/>
         <Layout>
