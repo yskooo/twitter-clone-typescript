951923a1271fa6bef16a400460a9876e19ebfa6b
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-04 22:07:19 +0800

    chore: add form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header"
+import Form from "../components/Form";
 
 export default function Home() {
   return (
     <>
      <Header showBackArrow label="Home" />
+     <Form placeholder="Whats happening?" />
     </>
   )
 }
