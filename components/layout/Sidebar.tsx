c46b5385b05ef3f526b0a4e1699a697f0f967f44
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-04 22:09:49 +0800

    update api route

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -25,7 +25,7 @@ const Sidebar = () => {
         },
         {
             label: 'Profile',
-            href: '/users/123',
+            href: `/users/${currentUser?.id}`,
             icon: FaUser,
             auth: true
         }
