54fdd719e26b1997b9265c179a029510d568fc05
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-05 21:30:53 +0800

    chore: display json post to ui

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -5,6 +5,9 @@ import { formatDistanceToNowStrict } from "date-fns";
 import useLoginModal from "../../hooks/useLoginModal";
 import useCurrentUser from "../../hooks/useCurrentUser";
 
+import Avatar from "../Avatar";
+import { AiOutlineMessage, AiOutlineHeart } from "react-icons/ai";
+
 interface PostItemProps {
     data: Record<string, any>;
     userId?: string;
@@ -52,10 +55,87 @@ const PostItem: React.FC<PostItemProps> = ({data, userId}) => {
         "
     >
      <div className="flex flex-row items-start gap-3">
-        
+        <Avatar userId={data.user.id} />
+        <div>
+            <div 
+                className="
+                    flex flex-row items-center gap-2
+                "
+            >
+                <p 
+                    onClick={goToUser}
+                    className="
+                        text-white
+                        font-semibold
+                        cursor-pointer
+                        hover:underline
+                    "
+                > {data.user.name} </p>
+                <span 
+                    onClick={goToUser}
+                    className="
+                      text-neutral-500
+                        cursor-pointer
+                        hover:underline
+                        hidden
+                        md:block
+                    "
+                >
+                    @{data.user.username}
+                </span>
+                <span 
+                    className="
+                    text-neutral-500
+                    text-sm
+                    "
+                >
+                    {createdAt}
+                </span>
+            </div>
+            <div className="text-white mt-1">
+                {data.body}
+            </div>
+            <div className="flex flex-row items-center mt-3 gap-10">
+                <div 
+                    className="
+                        flex
+                        flex-row
+                        items-center
+                        text-neutral-500
+                        gap-2
+                        cursor-pointer
+                        transition
+                        hover:text-sky-500
+                     "
+                    >
+                        <AiOutlineMessage size={20}/>
+                        <p>
+                            {data.comments?.length || 0}
+                        </p>
+                </div>
+                <div 
+                    onClick={onLike}
+                    className="
+                        flex
+                        flex-row
+                        items-center
+                        text-neutral-500
+                        gap-2
+                        cursor-pointer
+                        transition
+                        hover:text-red-500
+                     "
+                    >
+                        <AiOutlineHeart size={20}/>
+                        <p>
+                            {data.comments?.length || 0}
+                        </p>
+                </div>
+            </div>
+        </div>
      </div> 
     </div>
-  )
+  );
 }
 
 export default PostItem
