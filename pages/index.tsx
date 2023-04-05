4323a262d728b0532da9f7dbf04175d9dbdc47ac
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-05 21:30:18 +0800

    add postfeed component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import Form from "../components/Form";
+import PostFeed from "../components/posts/PostFeed";
 
 export default function Home() {
   return (
     <>
      <Header showBackArrow label="Home" />
      <Form placeholder="Whats happening?" />
+     <PostFeed/>
     </>
   )
 }
