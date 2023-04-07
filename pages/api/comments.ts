c711e8ab95c8960e5f39902a690068943b6c2b0c
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-07 16:29:32 +0800

    chore: optimize use comment notification

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -29,6 +29,35 @@ export default async function handler (
             }
         });
 
+        try {
+            const post = await prisma.post.findUnique({
+                where: {
+                    id: postId
+                }
+            });
+
+            if (post?.userId) {
+                await prisma.notification.create({
+                    data: {
+                        body: 'Someone replied to your tweet!',
+                        userId: post.userId
+                    }
+                });
+
+                await prisma.user.update({
+                    where: {
+                        id: post.userId
+                    },
+                    data: {
+                        hasNotification: true
+                    }
+                })
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+
         return res.status(200).json(comment);
         
     } catch (error) {
