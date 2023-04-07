2da04e78d9d95be6ea5e8ece03bea899150bdbc1
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-07 16:30:46 +0800

    chore: add alert sidebaritem

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router';
 import { useCallback } from 'react';
-import {IconType} from 'react-icons';
+import { IconType } from 'react-icons';
+import { BsDot } from 'react-icons/bs';
+
 import useCurrentUser from '../../hooks/useCurrentUser';
 import useLoginModal from '../../hooks/useLoginModal';
 
@@ -10,6 +12,7 @@ interface SidebarItemProps {
     icon: IconType
     onClick?: () => void;
     auth?: boolean;
+    alert?: boolean;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
@@ -17,7 +20,8 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
     href,
     icon: Icon,
     onClick,
-    auth
+    auth,
+    alert
 }) => {
   const loginModal = useLoginModal();
   const { data: currentUser } = useCurrentUser();
@@ -52,6 +56,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
             lg:hidden
         ">
             <Icon size={28} color="white" />
+            {alert ? <BsDot className='text-sky-500 absolute -top-4 left-0' size={70} /> : null}
       </div>
       <div className="
         relative
@@ -70,6 +75,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
         <p className="hidden lg:block text-white text-2xl">
           {label}
         </p>
+        {alert ? <BsDot className='text-sky-500 absolute -top-4 left-0' size={70} /> : null}
       </div>
     </div>
   )
