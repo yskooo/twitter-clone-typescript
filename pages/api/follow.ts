15ee66e1b0721212c2a62c2b949ac4609d41a6cc
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-07 16:29:12 +0800

    chore: optimized user follow notification

--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -35,6 +35,26 @@ export default async function handler (
 
     if (req.method === 'POST') {
         updatedFollowingIds.push(userId);
+
+        try {
+            await prisma.notification.create({
+                data: {
+                    body: 'Someone followed you!',
+                    userId
+                }
+            });
+
+            await prisma.user.update({
+                where: {
+                    id: userId
+                },
+                data: {
+                    hasNotification: true
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     if (req.method === 'DELETE') {
