c4fb9d817845767d95cae1d59f9fd1f651a74b26
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-07 16:28:51 +0800

    chore: optimized user like notification

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -34,6 +34,35 @@ export default async function handler (
 
         if (req.method === 'POST') {
             updatedLikedIds.push(currentUser.id);
+
+            try {
+                const post = await prisma.post.findUnique({
+                    where: {
+                        id: postId
+                    }
+                });
+
+                if (post?.userId) {
+                    await prisma.notification.create({
+                        data: {
+                            body: 'Someone liked your tweet!',
+                            userId: post.userId
+                        }
+                    });
+
+                    await prisma.user.update({
+                        where: {
+                            id: post.userId
+                        },
+                        data: {
+                            hasNotification: true
+                        }
+                    })
+                }
+
+            } catch (error) {
+                console.log(error);
+            }
         }
 
         if (req.method === 'DELETE') {
