22eee159092e16b596816ad8cf73ad9822c4ac85
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2023-04-07 13:38:50 +0800

    like.ts api

---
+++ pages/api/like.ts
@@ -0,0 +1,59 @@
+import { NextApiRequest, NextApiHandler, NextApiResponse } from "next";
+
+import serverAuth from "../../libs/serverAuth";
+import prisma from "../../libs/prismadb";
+
+export default async function handler (
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST' && req.method !== 'DELETE') {
+        return res.status(405).end();
+    }
+
+    try {
+        const { postId } = req.body;
+
+        const { currentUser } = await serverAuth(req);
+
+        if (!postId || typeof postId !== 'string') {
+            throw new Error('Invalid ID');
+        }
+
+        const post = await prisma.post.findUnique({
+            where: {
+                id: postId
+            }
+        });
+
+        if (!post) {
+            throw new Error('Invalid ID');
+        }
+
+        let updatedLikedIds = [...(post.likedIds || [])];
+
+        if (req.method === 'POST') {
+            updatedLikedIds.push(currentUser.id);
+        }
+
+        if (req.method === 'DELETE') {
+            updatedLikedIds = updatedLikedIds
+            .filter((likedId) => likedId !== currentUser?.id);
+        }
+        
+        const updatedPost = await prisma.post.update({
+            where: {
+                id: postId
+            },
+            data: {
+                likeIds: updatedLikedIds
+            }
+        });
+
+        return res.status(200).json(updatedPost);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).end();
+    }
+}
